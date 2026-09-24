c15ef29872df03c3b1b6683638270610fbcf6e9d
Akila Colambage <it25100931@my.sliit.lk>
2026-09-24 21:08:19 +0530

    Add LeaveBalance entity

---
+++ backend/src/main/java/entity/LeaveBalance.java
@@ -0,0 +1,25 @@
+package entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LEAVE_BALANCES")
+public class LeaveBalance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer balanceId;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id", nullable = false)
+    private Staff staff;
+
+    private Integer academicYear;
+
+    private Integer fullDayTotal;
+    private Integer shortLeaveTotal;
+
+    private Integer monthlyFullDayTotal;
+    private Integer monthlyShortLeaveTotal;
+
+    // getters/setters
+}
